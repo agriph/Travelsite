3254b3d1013ab0774f57bf0c4985fe5ed5d80eb6
Agnes Griph <agngriph@gmail.com>
2023-03-02 12:14:32 +0100

    Add files via upload

--- travelsite/myform.js
+++ travelsite/myform.js
@@ -1,17 +1,20 @@
-	class MyForm extends React.Component {
-	constructor(props){
-		super(props);
-		this.state = {
-			name: '',
-			apppVersion: ''
-		}
-	}
-	
-	render(){
-		return(
-		<div>
-		<h2>Hello!</h2>
-		<button>Downlad</button>
-		</div>
-		)
-	}
+function validateForm() {
+  let fnameValue = document.forms["aForm"]["fname"].value;
+  if (fnameValue == "") {
+    alert("First name is required.");
+    return false;
+  }
+
+  let lnameValue = document.forms["aForm"]["lname"].value;
+  if (lnameValue == "") {
+    alert("Last name is required.");
+    return false;
+  }
+  
+    let emailValue = document.forms["aForm"]["email"].value;
+  if (emailValue == "") {
+    alert("Your email is needed so that we can contact you.");
+    return false;
+  }
+
+}
